c014dcc8fc11bc0b772cb0c3f59f24441c1944e1
Noah Bayindirli 🥂 <noah@primeprotocol.xyz>
2025-03-18 14:27:46 -0400

    additional test coverage

--- contracts/stellar-axelar-gateway/src/tests/migrate.rs
+++ contracts/stellar-axelar-gateway/src/tests/migrate.rs
@@ -4,12 +4,44 @@ use stellar_axelar_std::{assert_auth, assert_err};
 
 use crate::contract::AxelarGateway;
 use crate::error::ContractError;
-use crate::storage::MessageApprovalValue;
+use crate::migrate::legacy_storage;
+use crate::storage::{self, MessageApprovalValue};
 use crate::tests::testutils::{
-    get_message_approval, setup_env, setup_legacy_message_approval, TestConfig,
+    setup_env, TestConfig,
 };
 
 const NEW_WASM: &[u8] = include_bytes!("testdata/stellar_axelar_gateway.optimized.wasm");
+
+#[test]
+fn migrate_succeeds() {
+    let TestConfig { env, client, .. } = setup_env(1, 5);
+
+    let owner = client.owner();
+
+    let new_wasm_hash = env.deployer().upload_contract_wasm(NEW_WASM);
+
+    let source_chain = String::from_str(&env, "source_chain");
+    let message_id = String::from_str(&env, "message_id");
+    let hash: BytesN<32> = BytesN::from_array(&env, &[1; 32]);
+
+    env.as_contract(&client.address, || {
+        let key = legacy_storage::MessageApprovalKey {
+            source_chain: source_chain.clone(),
+            message_id: message_id.clone(),
+        };
+
+        legacy_storage::set_message_approval(&env, key, &MessageApprovalValue::Approved(hash.clone()));
+    });
+
+    assert_auth!(owner, client.upgrade(&new_wasm_hash));
+
+    let migration_data = vec![&env, (source_chain, message_id)];
+
+    env.as_contract(&client.address, || {
+        let result = AxelarGateway::__migrate(&env, migration_data);
+        assert!(result.is_ok());
+    });
+}
 
 #[test]
 fn migrate_succeeds_with_valid_message_approvals() {
@@ -19,44 +51,47 @@ fn migrate_succeeds_with_valid_message_approvals() {
 
     let new_wasm_hash = env.deployer().upload_contract_wasm(NEW_WASM);
 
-    let source_chain1 = String::from_str(&env, "ethereum");
-    let message_id1 = String::from_str(&env, "message1");
-    let hash1: BytesN<32> = BytesN::from_array(&env, &[1; 32]);
+    let source_chain_1 = String::from_str(&env, "ethereum");
+    let message_id_1 = String::from_str(&env, "message1");
+    let hash: BytesN<32> = BytesN::from_array(&env, &[1; 32]);
 
-    let source_chain2 = String::from_str(&env, "polygon");
-    let message_id2 = String::from_str(&env, "message2");
+    let source_chain_2 = String::from_str(&env, "polygon");
+    let message_id_2 = String::from_str(&env, "message2");
 
     env.as_contract(&client.address, || {
-        setup_legacy_message_approval(
-            &env,
-            source_chain1.clone(),
-            message_id1.clone(),
-            MessageApprovalValue::Approved(hash1.clone()),
-        );
-        setup_legacy_message_approval(
-            &env,
-            source_chain2.clone(),
-            message_id2.clone(),
-            MessageApprovalValue::Executed,
-        );
+        let key_1 = legacy_storage::MessageApprovalKey {
+            source_chain: source_chain_1.clone(),
+            message_id: message_id_1.clone(),
+        };
+        legacy_storage::set_message_approval(&env, key_1, &MessageApprovalValue::Approved(hash.clone()));
+
+        let key_2 = legacy_storage::MessageApprovalKey {
+            source_chain: source_chain_2.clone(),
+            message_id: message_id_2.clone(),
+        };
+        legacy_storage::set_message_approval(&env, key_2, &MessageApprovalValue::Executed);
     });
 
     assert_auth!(owner, client.upgrade(&new_wasm_hash));
 
     let migration_data = vec![
         &env,
-        (source_chain1.clone(), message_id1.clone()),
-        (source_chain2.clone(), message_id2.clone()),
+        (source_chain_1.clone(), message_id_1.clone()),
+        (source_chain_2.clone(), message_id_2.clone()),
     ];
 
     assert_auth!(owner, client.migrate(&migration_data));
 
     assert_eq!(
-        get_message_approval(&env, &client.address, &source_chain1, &message_id1),
-        MessageApprovalValue::Approved(hash1)
+        env.as_contract(&client.address, || {
+            storage::message_approval(&env, source_chain_1, message_id_1)
+        }),
+        MessageApprovalValue::Approved(hash)
     );
     assert_eq!(
-        get_message_approval(&env, &client.address, &source_chain2, &message_id2),
+        env.as_contract(&client.address, || {
+            storage::message_approval(&env, source_chain_2, message_id_2)
+        }),
         MessageApprovalValue::Executed
     );
 }
@@ -69,28 +104,28 @@ fn migrate_fails_when_invalid_message_approval() {
 
     let new_wasm_hash = env.deployer().upload_contract_wasm(NEW_WASM);
 
-    let source_chain1 = String::from_str(&env, "ethereum");
-    let message_id1 = String::from_str(&env, "message1");
-    let hash1: BytesN<32> = BytesN::from_array(&env, &[1; 32]);
+    let source_chain_1 = String::from_str(&env, "ethereum");
+    let message_id_1 = String::from_str(&env, "message1");
+    let hash: BytesN<32> = BytesN::from_array(&env, &[1; 32]);
 
-    let source_chain2 = String::from_str(&env, "polygon");
-    let message_id2 = String::from_str(&env, "non_existent");
+    let source_chain_2 = String::from_str(&env, "polygon");
+    let message_id_2 = String::from_str(&env, "non_existent");
 
     env.as_contract(&client.address, || {
-        setup_legacy_message_approval(
-            &env,
-            source_chain1.clone(),
-            message_id1.clone(),
-            MessageApprovalValue::Approved(hash1.clone()),
-        );
+        let key = legacy_storage::MessageApprovalKey {
+            source_chain: source_chain_1.clone(),
+            message_id: message_id_1.clone(),
+        };
+
+        legacy_storage::set_message_approval(&env, key, &MessageApprovalValue::Approved(hash.clone()));
     });
 
     assert_auth!(owner, client.upgrade(&new_wasm_hash));
 
     let migration_data = vec![
         &env,
-        (source_chain1, message_id1),
-        (source_chain2, message_id2),
+        (source_chain_1, message_id_1),
+        (source_chain_2, message_id_2),
     ];
 
     assert_err!(
--- contracts/stellar-axelar-gateway/src/tests/testutils.rs
+++ contracts/stellar-axelar-gateway/src/tests/testutils.rs
@@ -1,7 +1,5 @@
-use soroban_sdk::{Address, Env, String};
+use soroban_sdk::Env;
 
-use crate::migrate::legacy_storage;
-use crate::storage::{self, MessageApprovalValue};
 use crate::testutils::{setup_gateway, TestSignerSet};
 use crate::AxelarGatewayClient;
 
@@ -20,29 +18,4 @@ pub fn setup_env<'a>(previous_signers_retention: u64, num_signers: u64) -> TestC
         signers,
         client,
     }
-}
-
-pub fn setup_legacy_message_approval(
-    env: &Env,
-    source_chain: String,
-    message_id: String,
-    value: MessageApprovalValue,
-) {
-    let key = legacy_storage::MessageApprovalKey {
-        source_chain,
-        message_id,
-    };
-
-    legacy_storage::set_message_approval(env, key, &value);
-}
-
-pub fn get_message_approval(
-    env: &Env,
-    contract_id: &Address,
-    source_chain: &String,
-    message_id: &String,
-) -> MessageApprovalValue {
-    env.as_contract(contract_id, || {
-        storage::message_approval(env, source_chain.clone(), message_id.clone())
-    })
 }
