9e116e23ef2100d74a683ffed781e8cbd4e7a222
TanvirDeol <tanvir@interoplabs.io>
2025-03-20 04:29:07 -0400

    small fix

--- contracts/stellar-multicall/src/interface.rs
+++ contracts/stellar-multicall/src/interface.rs
@@ -3,7 +3,6 @@ use soroban_sdk::{contractclient, Env, Val, Vec};
 use crate::error::ContractError;
 use crate::types::FunctionCall;
 
-#[contractclient(name = "MulticallClient")]
 pub trait MulticallInterface {
     /// Executes an arbitrary list of contract calls and returns the results of all the calls.
     ///
