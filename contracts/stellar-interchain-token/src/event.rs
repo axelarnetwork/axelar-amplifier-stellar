29c44bdf83f53a7d29fa4401f76c2583b790e88e
TanvirDeol <tanvir@interoplabs.io>
2025-03-24 15:31:36 -0400

    removed events

--- contracts/stellar-interchain-token/src/event.rs
+++ contracts/stellar-interchain-token/src/event.rs
@@ -9,30 +9,3 @@ pub struct MinterAddedEvent {
 pub struct MinterRemovedEvent {
     pub minter: Address,
 }
-
-#[derive(Debug, PartialEq, Eq, IntoEvent)]
-pub struct MintedEvent {
-    pub to: Address,
-    pub amount: i128,
-}
-
-#[derive(Debug, PartialEq, Eq, IntoEvent)]
-pub struct ApprovedEvent {
-    pub from: Address,
-    pub spender: Address,
-    pub amount: i128,
-    pub expiration_ledger: u32,
-}
-
-#[derive(Debug, PartialEq, Eq, IntoEvent)]
-pub struct TransferredEvent {
-    pub from: Address,
-    pub to: Address,
-    pub amount: i128,
-}
-
-#[derive(Debug, PartialEq, Eq, IntoEvent)]
-pub struct BurnedEvent {
-    pub from: Address,
-    pub amount: i128,
-}
