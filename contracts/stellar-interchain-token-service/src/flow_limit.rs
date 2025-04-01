2f31982d141822d3b6e05ca55b5da27745ea1b0f
TanvirDeol <tanvir@interoplabs.io>
2025-04-01 18:15:22 -0400

    addressed comments

--- contracts/stellar-interchain-token-service/src/flow_limit.rs
+++ contracts/stellar-interchain-token-service/src/flow_limit.rs
@@ -59,15 +59,9 @@ impl FlowDirection {
 
         let flow_amount = u128::try_from(flow_amount).expect("expected positive");
         let flow_limit = u128::try_from(flow_limit).expect("expected positive");
-        let flow = self.flow(env, token_id.clone());
 
-        let new_flow = flow
-            .checked_add(flow_amount)
-            .ok_or(ContractError::FlowAmountOverflow)?;
-        let max_allowed = self
-            .reverse_flow(env, token_id.clone())
-            .checked_add(flow_limit)
-            .ok_or(ContractError::FlowAmountOverflow)?;
+        let new_flow = self.flow(env, token_id.clone()) + flow_amount;
+        let max_allowed = self.reverse_flow(env, token_id.clone()) + flow_limit;
 
         // Equivalent to flow_amount + flow - reverse_flow <= flow_limit
         ensure!(new_flow <= max_allowed, ContractError::FlowLimitExceeded);
