41b2f452ac1aba525351c2a2ec97827903e3cc25
Tanvir Deol <tanvir@interoplabs.io>
2025-04-01 18:31:26 -0400

    feat(multicall): create multicall contract (#277)
    Co-authored-by: Milap Sheth <milap@interoplabs.io>

---
+++ contracts/stellar-multicall/src/interface.rs
@@ -0,0 +1,18 @@
+use stellar_axelar_std::{Env, Val, Vec};
+
+use crate::types::FunctionCall;
+
+//#[contractclient(name = "MulticallClient")]
+pub trait MulticallInterface {
+    /// Executes an arbitrary list of contract calls and returns the results of all the calls.
+    ///
+    /// # Arguments
+    /// * `function_calls` - A list of params containing the contract address, function name and arguments for each contract call.
+    ///
+    /// # Returns
+    /// - `Ok(Vec<Val>)`: Returns a vector with the return data of each function call
+    ///
+    /// # Errors:
+    /// - Propagates any error that occurs during the execution of the contract calls.
+    fn multicall(env: &Env, function_calls: Vec<FunctionCall>) -> Vec<Val>;
+}
---
+++ contracts/stellar-multicall/src/lib.rs
@@ -0,0 +1,21 @@
+#![no_std]
+
+#[cfg(test)]
+extern crate std;
+
+pub mod interface;
+
+pub mod types;
+
+#[cfg(test)]
+mod tests;
+
+cfg_if::cfg_if! {
+    if #[cfg(all(feature = "library", not(test)))] {
+        pub use interface::{MulticallInterface};
+    } else {
+        mod contract;
+
+        pub use contract::{Multicall, MulticallClient};
+    }
+}
