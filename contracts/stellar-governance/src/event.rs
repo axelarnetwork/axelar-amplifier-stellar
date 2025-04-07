fa60df4ec801b7d561a2453b5e8860f1b06b1c89
TanvirDeol <tanvir@interoplabs.io>
2025-04-07 10:00:13 -0400

    main commit

---
+++ contracts/stellar-governance/src/event.rs
@@ -0,0 +1,52 @@
+use core::fmt::Debug;
+
+use stellar_axelar_std::{ Address, Bytes, IntoEvent};
+
+#[derive(Debug, PartialEq, Eq, IntoEvent)]
+pub struct ProposalScheduledEvent {
+    pub target: Address,
+    pub eta: u64,
+    pub proposal_hash: Bytes,
+    #[data]
+    pub call_data: Bytes,
+}
+
+#[derive(Debug, PartialEq, Eq, IntoEvent)]
+pub struct ProposalCancelledEvent {
+    pub target: Address,
+    pub proposal_hash: Bytes,
+    #[data]
+    pub call_data: Bytes,
+}
+
+#[derive(Debug, PartialEq, Eq, IntoEvent)]
+pub struct ProposalExecutedEvent {
+    pub target: Address,
+    pub proposal_hash: Bytes,
+    #[data]
+    pub call_data: Bytes,
+}
+
+#[derive(Debug, PartialEq, Eq, IntoEvent)]
+pub struct OperatorProposalApprovedEvent {
+    pub target: Address,
+    pub proposal_hash: Bytes,
+    #[data]
+    pub call_data: Bytes,
+}
+
+#[derive(Debug, PartialEq, Eq, IntoEvent)]
+pub struct OperatorProposalCancelledEvent {
+    pub target: Address,
+    pub proposal_hash: Bytes,
+    #[data]
+    pub call_data: Bytes,
+}
+
+#[derive(Debug, PartialEq, Eq, IntoEvent)]
+pub struct OperatorProposalExecutedEvent {
+    pub target: Address,
+    pub proposal_hash: Bytes,
+    #[data]
+    pub call_data: Bytes,
+}
---
+++ contracts/stellar-governance/src/lib.rs
@@ -0,0 +1,27 @@
+#![no_std]
+
+#[cfg(any(test, feature = "testutils"))]
+extern crate std;
+
+mod error;
+mod interface;
+mod types;
+
+#[cfg(test)]
+mod tests;
+
+cfg_if::cfg_if! {
+    if #[cfg(all(feature = "library", not(feature = "testutils")))] {
+        pub use interface::StellarGovernanceClient;
+        pub use interface::StellarGovernanceInterface;
+    } else {
+        mod contract;
+        mod event;
+        mod storage;
+        mod timelock;
+
+        pub use crate::contract::StellarGovernance;
+        pub use crate::error::ContractError;
+        pub use crate::interface::StellarGovernanceInterface;
+    }
+}
